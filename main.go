28e273eb004bd2b35b2e271cbdb1814719c401b4
huypl53 <phamlehuy53@gmail.com>
2023-09-30 22:22:22 +0700

    messenger init

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 
-	report "kampuni/scheduler/internal/cronjob"
-	"kampuni/scheduler/pkg/db"
+	// report "kampuni/scheduler/internal/cronjob"
+	// "kampuni/scheduler/pkg/db"
+	"bufio"
+	"io"
+	"kampuni/scheduler/pkg/messenger"
 
 	"github.com/joho/godotenv"
 )
@@ -14,10 +18,50 @@ func main() {
 	if err != nil {
 		fmt.Println("Error loading .env file")
 	}
+	//
+	// if err := db.Run(); err != nil {
+	// 	panic(err)
+	// }
+	//
+	// report.ReportDaily()
 
-	if err := db.Run(); err != nil {
-		panic(err)
-	}
+  broker := messenger.Broker{}
+  defer broker.Close()
 
-	report.ReportDaily()
+  topic := "kampuni.scheduler"
+  broker.Init("amqp://guest:guest@localhost:5672/")
+  broker.PublishOn(topic, read(os.Stdin))
+  broker.ReceiveOn(topic, write(os.Stdout))
+
+  // forever := make(chan struct{})
+  var forever chan struct{}
+  <- forever
 }
+
+// These 2 func are for testing
+// read is this application's translation to the message format, scanning from
+// stdin.
+func read(r io.Reader) <-chan messenger.Message {
+	lines := make(chan messenger.Message )
+	go func() {
+		defer close(lines)
+		scan := bufio.NewScanner(r)
+		for scan.Scan() {
+			lines <- scan.Bytes()
+		}
+	}()
+	return lines
+}
+
+// write is this application's subscriber of application messages, printing to
+// stdout.
+func write(w io.Writer) chan<- messenger.Message  {
+	lines := make(chan messenger.Message )
+	go func() {
+		for line := range lines {
+			fmt.Fprintln(w, "Receive " + string(line))
+		}
+	}()
+	return lines
+}
+
--- pkg/messenger/messenger.go
+++ pkg/messenger/messenger.go
@@ -2,101 +2,228 @@ package messenger
 
 import (
   amqp "github.com/rabbitmq/amqp091-go"
+  "context"
+  "crypto/sha1"
+  "os"
+  "log"
+  "fmt"
+  "time"
 )
 
+
 type Messenger interface{
-  Init()
+  Init(url string)
+  PublishOn(topic string, messages <- chan Message)
+  ReceiveOn(topic string, messages chan<- Message)
   Close()
 }
 
-type Broker struct {
-  connection amqp.Connection
-  channel amqp.Channel
-  queue amqp.Queue
-
-  publisher Publisher
-  consumer Consumer
+type Broker struct{
+  url string
+  ctx context.Context
+  done context.CancelFunc
 }
 
-type Publisher struct{
 
-}
-type Consumer struct {
-  
-}
+// exchange binds the publishers to the subscribers
+const exchange = "topic"
 
-// TODO: config with .env
-func Init(url string) *Broker{
-  conn, err := amqp.Dial("amqp://guest:guest@10.16.208.94:5672/")
-  failOnError(err, "Failed to connect to RabbitMQ")
-  if !err {
-    ch, err := conn.Channel()
-    failOnError(err, "Failed to open a channel")
+// Message is the application type for a Message.  This can contain identity,
+// or a reference to the receiver chan for further demuxing.
+type Message []byte
 
-    q, queueErr := ch.QueueDeclare(
-      "hello", // name
-      false,   // durable
-      false,   // delete when unused
-      false,   // exclusive
-      false,   // no-wait
-      nil,     // arguments
-    )
-    failOnError(queueErr, "Failed to declare a queue")
-
-    return &Broker{conn, ch, q}
-  }
-  return &Broker{}
+// session composes an amqp.Connection with an amqp.Channel
+type session struct {
+	*amqp.Connection
+	*amqp.Channel
 }
 
-func (broker *Broker)Close() {
-// TODO
-  broker.connection.Close()
-  broker.channel.Close()
+func (broker *Broker)Init(url string){
+  broker.url = url
+  ctx, done := context.WithCancel(context.Background())
+  broker.ctx = ctx
+  broker.done = done
 }
 
-func (broker *Broker) receiveMessage(){
-  broker.consumer.receive()
+func (broker *Broker)Close(){
+  broker.done()
 }
 
-func (broker *Broker) sendMessage(msg string) {
-
-}
-
-func Init(channel *amqp.Channel, queueName string, args...) *Consumer{
-  msgs, err := channel.Consume(
-    q.Name, // queue
-    "",     // consumer
-    true,   // auto-ack
-    false,  // exclusive
-    false,  // no-local
-    false,  // no-wait
-    nil,    // args
-  )
-  failOnError(err, "Failed to register a consumer")
-
-  var forever chan struct{}
-
-  go func() {
-    for d := range msgs {
-      log.Printf("Received a message: %s", d.Body)
-    }
+func (broker *Broker)PublishOn(topic string, messages <- chan Message){
+  go func(){
+    publish(redial(broker.ctx, broker.url),topic, messages)
+    broker.done()
   }()
 }
-func (consumer *Consumer) receive(){
 
+func (broker *Broker)ReceiveOn(topic string, messages chan <- Message){
+  go func(){
+    subscribe(redial(broker.ctx, broker.url),topic, messages)
+    broker.done()
+  }()
 }
 
-func (publisher *Publisher) send(){
+
+// Close tears the connection down, taking the channel with it.
+func (s session) Close() error {
+	if s.Connection == nil {
+		return nil
+	}
+	return s.Connection.Close()
 }
 
+// redial continually connects to the URL, exiting the program when no longer possible
+func redial(ctx context.Context, url string) chan chan session {
+	sessions := make(chan chan session)
 
+	go func() {
+		sess := make(chan session)
+		defer close(sessions)
 
-log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+		for {
+			select {
+			case sessions <- sess:
+			case <-ctx.Done():
+				log.Println("shutting down session factory")
+				return
+			}
 
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Panicf("%s: %s", msg, err)
+			conn, err := amqp.Dial(url)
+			if err != nil {
+				log.Fatalf("cannot (re)dial: %v: %q", err, url)
+			}
+
+			ch, err := conn.Channel()
+			if err != nil {
+				log.Fatalf("cannot create channel: %v", err)
+			}
+
+			if err := ch.ExchangeDeclare(exchange, "fanout", false, true, false, false, nil); err != nil {
+				log.Fatalf("cannot declare fanout exchange: %v", err)
+			}
+
+			select {
+			case sess <- session{conn, ch}:
+        // log.Println("Created session: ", conn, ch)
+			case <-ctx.Done():
+				log.Println("shutting down new session")
+				return
+			}
+		}
+	}()
+
+	return sessions
+}
+
+// publish publishes messages to a reconnecting session to a fanout exchange.
+// It receives from the application specific source of messages.
+func publish(sessions chan chan session, topic string, messages <-chan Message) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for session := range sessions {
+		var (
+			running bool
+			reading = messages
+			pending = make(chan Message, 1)
+			confirm = make(chan amqp.Confirmation, 1)
+		)
+
+		pub := <-session
+
+		// publisher confirms for this channel/connection
+		if err := pub.Confirm(false); err != nil {
+			log.Printf("publisher confirms not supported")
+			close(confirm) // confirms not supported, simulate by always nacking
+		} else {
+			pub.NotifyPublish(confirm)
+		}
+
+		log.Printf("publishing...")
+
+	Publish:
+		for {
+			var body Message
+			select {
+			case confirmed, ok := <-confirm:
+				if !ok {
+					break Publish
+				}
+				if !confirmed.Ack {
+					log.Printf("nack message %d, body: %q", confirmed.DeliveryTag, string(body))
+				}
+				reading = messages
+
+			case body = <-pending:
+				// routingKey := "ignored for fanout exchanges, application dependent for other exchanges"
+				routingKey := topic
+				err := pub.PublishWithContext(ctx, exchange, routingKey, false, false, amqp.Publishing{
+					Body: body,
+				})
+				// Retry failed delivery on the next session
+				if err != nil {
+					pending <- body
+					pub.Close()
+					break Publish
+				}
+
+			case body, running = <-reading:
+				// all messages consumed
+        fmt.Println("publish: ", string( body ))
+				if !running {
+					return
+				}
+				// work on pending delivery until ack'd
+				pending <- body
+				reading = nil
+			}
+		}
 	}
 }
 
-<-forever
+// identity returns the same host/process unique string for the lifetime of
+// this process so that subscriber reconnections reuse the same queue name.
+func identity() string {
+	hostname, err := os.Hostname()
+	h := sha1.New()
+	fmt.Fprint(h, hostname)
+	fmt.Fprint(h, err)
+	fmt.Fprint(h, os.Getpid())
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+// subscribe consumes deliveries from an exclusive queue from a fanout exchange and sends to the application specific messages chan.
+func subscribe(sessions chan chan session, topic string, messages chan<- Message) {
+	queue := identity()
+
+	for session := range sessions {
+		sub := <-session
+
+		if _, err := sub.QueueDeclare(queue, false, true, true, false, nil); err != nil {
+			log.Printf("cannot consume from exclusive queue: %q, %v", queue, err)
+			return
+		}
+
+		// routingKey := "application specific routing key for fancy topologies"
+		routingKey := topic
+    log.Println("Receive routingKey: ", routingKey)
+		if err := sub.QueueBind(queue, routingKey, exchange, false, nil); err != nil {
+			log.Printf("cannot consume without a binding to exchange: %q, %v", exchange, err)
+			return
+		}
+
+		deliveries, err := sub.Consume(queue, "", false, true, false, false, nil)
+		if err != nil {
+			log.Printf("cannot consume from: %q, %v", queue, err)
+			return
+		}
+
+		log.Printf("subscribed...")
+
+		for msg := range deliveries {
+			messages <- msg.Body
+			sub.Ack(msg.DeliveryTag, false)
+		}
+	}
+}
+
